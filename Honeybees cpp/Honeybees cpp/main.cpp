783f5fb600c1042cad85c83730ce73b7dc2380ee
JasperDre <j.drescher@cortexarts.com>
2016-04-04 21:29:35 +0200

    added a method to draw triangles

--- Honeybees cpp/Honeybees cpp/main.cpp
+++ Honeybees cpp/Honeybees cpp/main.cpp
@@ -8,11 +8,31 @@ void changeViewPort(int w, int h)
 	glViewport(0, 0, w, h);
 }
 
-void render()
-{
+void drawHexagon(float x, float y, float size) {
+	glBegin(GL_TRIANGLES);
+	glVertex2f((-0.5f*size) + x, (-0.5f*size) + y);
+	glVertex2f((0.0f*size) + x, (0.5f*size) + y);
+	glVertex2f((0.5f*size) + x, (-0.5f*size) + y);
+	/*glVertex2f(-0.1f, -.1f);
+	glVertex2f(0.1f, 0.1f);
+	glVertex2f(0.0f, 0.0f);
+	glVertex2f(0.0f, .5f);
+	glVertex2f(.5f, 0.0f);
+	for (float index = 0; index < 6; index++)
+	{
+		glVertex2f((index) / 3.0f, 0.8f);
+	}*/
+	//glEnd();
+}
+
+void render() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glutSwapBuffers();
+	glLoadIdentity();
+	drawHexagon(-.5f, 0.0f, 1.0f);
+	drawHexagon(.5f, 0.0f, 1.0f);
 }
+
 
 int main(int argc, char* argv[]) {
 	glutInit(&argc, argv);
@@ -31,5 +51,3 @@ int main(int argc, char* argv[]) {
 	glutMainLoop();
 	return 0;
 }
-
-
