5134d0ca75631bd3b29d8ec3672843e7081c89c5
JasperDre <jasperdre@gmail.com>
2016-04-10 17:41:26 +0200

    added proper drawhexagon parameters

--- Honeybees cpp/Honeybees cpp/main.cpp
+++ Honeybees cpp/Honeybees cpp/main.cpp
@@ -4,27 +4,27 @@
 #include <iostream>
 
 void drawHexagon(float x, float y, float size) {
-	/* Set drawing color */
+	/* Set drawing color (RGBA)*/
 	glColor3d(0.957, 0.643, 0.376);
 	/* Tell OpenGL your intentions */
 	glBegin(GL_POLYGON);
-	/* Set the vertices */
-	glVertex2d(-0.5 + x * size, 0.5 + y * size);
-	glVertex2d(-0.5 + x * size, -0.5 + y * size);
-	glVertex2d(0 + x * size, -0.8 + y * size);
-	glVertex2d(0.5 + x * size, -0.5 + y * size);
-	glVertex2d(0.5 + x * size, 0.5 + y * size);
-	glVertex2d(0 + x * size, 0.8 + y * size);
+	/* Set the vertices glVertex2d(x, y);*/
+	glVertex2d(-0.5 * size + x, 0.5 * size + y);
+	glVertex2d(-0.5 * size + x, -0.5 * size + y);
+	glVertex2d(0 * size + x, -0.8 * size + y);
+	glVertex2d(0.5 * size + x, -0.5 * size + y);
+	glVertex2d(0.5 * size + x, 0.5 * size + y);
+	glVertex2d(0 * size + x, 0.8 * size + y);
 	/* Done making polygon */
 	glEnd();
 }
 void display(void) {
-	/* Set background color (green) */
+	/* Set background color (RGBA) */
 	glClearColor(0.565, 0.933, 0.565, 0);
 	glClear(GL_COLOR_BUFFER_BIT);
-	/* draw the honey*/
-	drawHexagon(-1.0f, -0.4f, 0.4f);
-	drawHexagon(0.9f, 0.4f, 0.4f);
+	/* draw the honey (x,y,size)*/
+	drawHexagon(-0.5f, -0.4f, 0.5f);
+	drawHexagon(0.5f, 0.4f, 0.7f);
 	/* Clear screen and draw */
 	glutSwapBuffers();
 }
