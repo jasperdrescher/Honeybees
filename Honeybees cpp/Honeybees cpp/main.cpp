2716f887163f406351e73657653a80d71f36309f
JasperDre <jasperdre@gmail.com>
2016-04-10 15:23:36 +0200

    added void paramters, however not working properly.

--- Honeybees cpp/Honeybees cpp/main.cpp
+++ Honeybees cpp/Honeybees cpp/main.cpp
@@ -3,23 +3,28 @@
 #include <GL/freeglut.h>
 #include <iostream>
 
-void display(void) {
-	/* Set background color (green) */
-	glClearColor(0.565, 0.933, 0.565, 0);
-	glClear(GL_COLOR_BUFFER_BIT);
+void drawHexagon(float x, float y, float size) {
 	/* Set drawing color */
 	glColor3d(0.957, 0.643, 0.376);
 	/* Tell OpenGL your intentions */
 	glBegin(GL_POLYGON);
 	/* Set the vertices */
-	glVertex2d(-0.5, 0.5);
-	glVertex2d(-0.5, -0.5);
-	glVertex2d(0, -0.8);
-	glVertex2d(0.5, -0.5);
-	glVertex2d(0.5, 0.5);
-	glVertex2d(0, 0.8);
+	glVertex2d(-0.5 + x * size, 0.5 + y * size);
+	glVertex2d(-0.5 + x * size, -0.5 + y * size);
+	glVertex2d(0 + x * size, -0.8 + y * size);
+	glVertex2d(0.5 + x * size, -0.5 + y * size);
+	glVertex2d(0.5 + x * size, 0.5 + y * size);
+	glVertex2d(0 + x * size, 0.8 + y * size);
 	/* Done making polygon */
 	glEnd();
+}
+void display(void) {
+	/* Set background color (green) */
+	glClearColor(0.565, 0.933, 0.565, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	/* draw the honey*/
+	drawHexagon(-1.0f, -0.4f, 0.4f);
+	drawHexagon(0.9f, 0.4f, 0.4f);
 	/* Clear screen and draw */
 	glutSwapBuffers();
 }
