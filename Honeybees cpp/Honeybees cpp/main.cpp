84333a06d47434a5231ea95bbee22885b936523b
JasperDre <j.drescher@cortexarts.com>
2016-04-11 21:24:57 +0200

    Array for hexagon positions and sort of working collision detection

--- Honeybees cpp/Honeybees cpp/main.cpp
+++ Honeybees cpp/Honeybees cpp/main.cpp
@@ -1,14 +1,14 @@
-/*
-Do I need to have a float in a float parameter?
-Why is the randomFloat function updated after every interaction and how do I fix it?
-*/
 #include <Windows.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <iostream>
+#include <stdlib.h>
+
+float currentHexagonPos[6][2];
+bool displayConfigured = false;
 
 float randomFloat(float a, float b) {
-	float random = ((float)rand()) / (float)RAND_MAX;
+	float random = ((float)rand() / (float)RAND_MAX);
 	float diff = b - a;
 	float r = random * diff;
 	return a + r;
@@ -29,13 +29,36 @@ void drawHexagon(float x, float y, float size) {
 	/* Done making polygon */
 	glEnd();
 }
+
+void setupHexagons() {
+	for (int hexagonCount = 0; hexagonCount < 6; hexagonCount++)
+	{
+		currentHexagonPos[hexagonCount][0] = randomFloat(-1.0f, 1.0f);
+		currentHexagonPos[hexagonCount][1] = randomFloat(-1.0f, 1.0f);
+	}
+}
+
+bool doesCollide(float hexagon1[], float hexagon2[]) {
+	float diffX = hexagon1[0] - hexagon2[0];
+	float diffY = hexagon1[1] - hexagon2[1];
+	float distance = sqrtf((diffX * diffX) + (diffY * diffY));
+	return distance < 0.5f;
+}
+
 void display(void) {
+	if (!displayConfigured) {
+		// pre-configure  game layout
+		setupHexagons();
+		displayConfigured = true;
+	}
 	/* Set background color (RGBA) */
 	glClearColor(0.565, 0.933, 0.565, 0);
 	glClear(GL_COLOR_BUFFER_BIT);
+
 	/* draw the honey (x,y,size)*/
-	drawHexagon(randomFloat(0.0f, 1.0f), -0.4f, 0.5f);
-	drawHexagon(0.5f, 0.4f, 0.7f);
+	for (int hexagonCount = 0; hexagonCount < 6; hexagonCount++) {
+		drawHexagon(currentHexagonPos[hexagonCount][0], currentHexagonPos[hexagonCount][1], 0.2f);
+	}
 	/* Clear screen and draw */
 	glutSwapBuffers();
 }
